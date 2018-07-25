8f60ad87443e4e023c43668ffebe6bd3b60ba090
mydongistiny <jaysonedson@gmail.com>
2018-07-24 23:32:07 -0700

    os9: Bring in a bare vendor to start
    Change-Id: Ifa5c8a62b17f4d40eeb927dbf62cc81b39144bfb
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

---
+++ config/common.mk
@@ -0,0 +1,9 @@
+# os9 Rom 
+#
+PRODUCT_PACKAGE_OVERLAYS += vendor/os9/overlay/common
+
+# Keyboard libs
+PRODUCT_COPY_FILES += \
+    vendor/os9/prebuilt/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_latinimegoogle.so
+
+
