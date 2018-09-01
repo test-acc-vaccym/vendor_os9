d9c38d6cc192fe8e11d1ccb33b50e0ea5efea6b4
Bamavic <bamavic1971@gmail.com>
2018-09-01 10:43:27 -0400

    add products

---
+++ products/os9_marlin.mk
@@ -0,0 +1,38 @@
+#
+# Copyright 2018 OS-9
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common os9 stuff.
+$(call inherit-product, vendor/os9/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/marlin/aosp_marlin.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := marlin
+PRODUCT_NAME := os9_marlin
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel XL
+PRODUCT_MANUFACTURER := Google
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+# Device Fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=marlin \
+    PRIVATE_BUILD_DESC="marlin-user 9 PPR1.180610.009 4898911 release-keys" \
+    BUILD_FINGERPRINT="google/marlin/marlin:9/PPR1.180610.009/4898911:user/release-keys"
