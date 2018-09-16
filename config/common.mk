245b8ef371c0903c66791f255323c10dffa13594
Bamavic <bamavic1971@gmail.com>
2018-09-16 17:19:15 -0400

    add bootanimation

--- config/common.mk
+++ config/common.mk
@@ -6,4 +6,6 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/os9/overlay/common
 PRODUCT_COPY_FILES += \
     vendor/os9/prebuilt/lib64/libjni_latinimegoogle.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libjni_latinimegoogle.so
 
+$(call inherit-product, vendor/os9/config/main.mk)
+
 
