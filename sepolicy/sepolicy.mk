BenzoRomGSI/vendor_benzo

sepolicy: Allow setting service.adb.tcp.* properties
Required for network adb enable/disable to function

Change-Id: I4b3a4d1234a58057ee7f9baf2455a516df7f57d3
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

sepolicy/sepolicy.mk
@@ -1,0 +1,5 @@
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/benzo/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/benzo/sepolicy/private

test-acc-vaccym/vendor_os9

sepolicy/sepolicy.mk
@@ -1,0 +1,5 @@
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/os9/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/os9/sepolicy/private